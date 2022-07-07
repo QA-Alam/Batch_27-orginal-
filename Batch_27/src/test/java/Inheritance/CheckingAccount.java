24a9cf3f932d068ae5a3d1749c7fd8ef27bfbeea
QA-alam <alam.itprofessional@gmail.com>
2022-07-06 22:22:51 -0400

    This is pussing by alam

--- Batch_27/src/test/java/Inheritance/CheckingAccount.java
+++ Batch_27/src/test/java/Inheritance/CheckingAccount.java
@@ -1,25 +1,27 @@
 package Inheritance;
 
 public class CheckingAccount {
-//we are not intialize any value at the class level 
-//and instance level variable becasue of we need to reuse those variable
-	
-	//declaring all the variable
-	static int balance; 
+//we are not initialize any value at the class level 
+//and instance level variable because of we need to reuse those variable
+
+// declaring all the variable
+	static int balance;
 	static int withdraw;
 	static int deposit;
 	static String accountName = "Turikul Islam";
 	static int total;
+	String name;
 
-	
-	
- protected void accountActivity() {
+	protected void accountActivity() {
+		balance = 1500;
+		withdraw = 100;
+		deposit = 9999;
+		total = balance - withdraw + deposit;
+		System.out.println(accountName + " total checking account balance $" + total);
+	}
 
-	balance = 1500;
-	withdraw = 100;
-	deposit = 9999;
-	total = balance-withdraw+deposit;
-	System.out.println(accountName + " total checking account balance $"+total);
- }
- 
+	public void getName() {
+		name = "Abul Hashem";
+		System.out.println(name);
+	}
 }
--- Batch_27/src/test/java/Inheritance/SavingsAccount.java
+++ Batch_27/src/test/java/Inheritance/SavingsAccount.java
@@ -5,13 +5,17 @@ public class SavingsAccount extends CheckingAccount {
 	
 public static void main(String[] args) {
 	
+	// if i create an object with parent class then we are getting the only parent class properties 
+	CheckingAccount obj1 = new CheckingAccount(); 
+	obj1.accountActivity();
+	
+
+	 // Create a object child class than we are getting both class properties 
 	SavingsAccount obj = new SavingsAccount();
 	obj.accountActivity();
 	obj.savingActivity();
 	
-	
 }
-	
 	
 	public  void savingActivity() {
 		//System.out.println(CheckingAccount.accountName);
